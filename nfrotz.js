5470c22d91db8b0166b2f50bb3cfcb7eee90d99c
bitbxl <claudiogizzi@tiscali.it>
2021-04-11 20:54:30 +0200

    Reworked filters

--- nfrotz.js
+++ nfrotz.js
@@ -44,7 +44,6 @@ function nfrotz(options) {
                 if (gf)
                     dfargs.push(gf);
 
-                console.log(gf);
 
                 this.dfrotz = spawn(path.join(__dirname, this.options.dfexec), dfargs);
 
@@ -55,7 +54,7 @@ function nfrotz(options) {
                     while (null != (chunk = this.dfrotz.stdout.read())) {
                         chunks = chunks + chunk.toString();
                     }
-                    this.state = 'running';
+                    state = 'running';
                     resolve(chunks);
                 });
 
@@ -68,14 +67,51 @@ function nfrotz(options) {
     }
 
 
-    this.command = function(command) {
+    this.command = function(...commands) {
 
         return new Promise((resolve, reject) => {
 
             if (state === 'running') {
 
+                var responses = [];
 
+                var listener = () => {
 
+                    let chunk;
+                    let chunks = '';
+
+                    while (null != (chunk = this.dfrotz.stdout.read())) {
+                        chunks = chunks + chunk.toString();
+                    }
+                    /*
+                                        responses.push(chunks
+                                            .replace(/(^\s+)|(\s+$)/g, '') // trailing spaces /NW/CR
+                                            .replace(/(\w)(\n)(\w)/g, '$1 $3') // delete single /NW/CR inside paragraph
+                                            .replace(/\n{2,}/g, '\n') // reduce multiple NW/CR to one
+                                            .replace(/\s\w*>$/g, '') // remove trailing cursor at the end 
+                                        );
+                    */
+
+                    responses.push(this._filters[this.options.filter](chunks));
+
+                    if (responses.length == commands.length) {
+                        this.dfrotz.stdout.removeListener('readable', listener);
+
+                        if (responses.length == 1) {
+                            resolve(responses[0]);
+                        } else {
+                            resolve(responses);
+                        }
+
+                    }
+
+                }
+
+                this.dfrotz.stdout.on('readable', listener);
+
+                commands.forEach((command, index) => {
+                    this.dfrotz.stdin.write(command + '\n');
+                });
 
 
             } else {
@@ -86,20 +122,39 @@ function nfrotz(options) {
 
 
         });
+
+
+        function objectifyResp(commands, responses) {
+
+            var arr = [];
+
+            commands.forEach((command, index) => {
+                let obj = {
+                    'index': index,
+                    'command': command,
+                    'response': response[index]
+                }
+                arr.push(obj);
+            });
+
+            return arr
+
+        }
+
+
+
     }
-
-
-
 
 
     function validatedOpts(options) {
         options = options || {};
         options.dfexec = options.dfexec || './frotz/dfrotz';
-        options.dfopts = options.dfopts || [];
+        options.dfopts = options.dfopts || ['-m'];
         options.gamefile = options.gamefile || null;
         options.savepath = options.savepath || './saves';
-        options.filter = options.filter || null;
-
+        options.filter = options.filter || 'compact';
+	
+	
         state = _.every(_.omit(options, ['dfopts', 'filter']), function(opt) {
             return !_.isNull(opt)
         }) ? 'ready' : 'idle';
@@ -107,23 +162,46 @@ function nfrotz(options) {
         return options;
     }
 
-
 }
+
+
+nfrotz.prototype._filters = {};
+nfrotz.prototype._filters.compact = function(str) {
+    return str
+        .replace(/(^\s+)|(\s+$)/g, '') // trailing spaces /NW/CR
+        .replace(/(\w)(\n)(\w)/g, '$1 $3') // delete single /NW/CR inside paragraph
+        .replace(/\n{2,}/g, '\n') // reduce multiple NW/CR to one
+        .replace(/\s\w*>$/g, ''); // remove trailing cursor at the end
+};
+nfrotz.prototype._filters.none = function(str) {
+    return str
+};
+
+
+
 
 
 var nf = new nfrotz();
 
 
+
 var opts = {
-    gamefile: '../Ruins.z5'
+    gamefile: '../Ruins/Ruins.z5',
+    filter: 'compact'
 };
 
 
 (async () => {
 
+
     await nf.init(opts);
+
     var res = await nf.start();
-    console.log(res);
+    //console.log(res);
+
+    var cres = await nf.command('', "look", "look ground", "take mushroom", "quit", "yes");
+    console.log(JSON.stringify(cres, null, '\t'));
+
 
 })().catch(e => {
     console.error(e)
